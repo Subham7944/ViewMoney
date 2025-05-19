63386df1312a2836e1d1093eb542accebc831d18
Subham <ssingh.ss8271@gmail.com>
2025-05-19 23:12:21 +0530

    Create new and account data

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,9 +16,10 @@ export default function RootLayout({ children }) {
     <ClerkProvider>
       <html lang="en">
         <body className={`${inter.className}`}>
-          <Header />
+          <Header richColors />
           {/* Add top margin to prevent content from being hidden behind the Header */}
           <main className="min-h-screen mt-20">{children}</main>
+          <Toaster />
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text text-center text-gray-600">
               <p>Made with Love</p>
